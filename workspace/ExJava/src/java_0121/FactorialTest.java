768e4e4296ac809f6977897824775691cc7ba5b1
kh <jjanggu1990@gmail.com>
2016-01-21 20:54:28 +0900

    Factorialtest.java 추가

---
+++ workspace/ExJava/src/java_0121/FactorialTest.java
@@ -0,0 +1,23 @@
+package java_0121;
+
+class FactorialTest {
+
+	public static void main(String[] args) {
+		System.out.println(factorial(4));
+
+	}
+	static long factorial(int n){
+		long result = 0;
+		if(n==1){
+			result = 1;
+			System.out.println("factorial -> if "+result);
+
+		}else{
+			System.out.println("factorial -> else " +result);
+			result = n * factorial(n-1);
+		}
+		System.out.println("return ¾Õ : "+result);
+		return result;
+	}
+
+}
