d34cd9cd39f7f316bb2f19fb0b1195ca40726986
kh <jjanggu1990@gmail.com>
2016-02-01 21:05:22 +0900

    0201 마지막 커밋

--- workspace/ExJava/src/java_0201/Test.java
+++ workspace/ExJava/src/java_0201/Test.java
@@ -3,22 +3,40 @@ package java_0201;
 class Test {
 
 	public static void main(String[] args) {
-		String a = "a";
-		String b = "a";
-		if(a==b){
-			System.out.println(true);
+//		String a = new String ("abcas");
+//		String b = new String ("abcas");
+//		System.out.println(a.hashCode());
+//		System.out.println(b.hashCode());
+//		String c = "abcbc";
+//		String d = "abcbc";
+//		System.out.println(c.hashCode());
+//		System.out.println(d.hashCode());
+//		
+//		
+//		String a1 = null;
+//		System.out.println(a1);
+//		System.out.println(a1+1);
+//
+		long [] arr = new long [10];
+		for(int j = 0;j<10;j++){
+			long a = System.currentTimeMillis();
+			for(int i = 0 ; i<10000000;i++){
+
+//				String str = Integer.toString(i);
+				String str = String.valueOf(i);
+			}
+			long b = System.currentTimeMillis();
+			arr[j] = b-a;
 		}
-		else{
-			System.out.println(false);
+		int sum =0;
+		for(int i =0;i<10;i++){
+			sum += arr[i];
 		}
-		String c = new String("a");
-		String d = new String("a");
+		System.out.println(sum/10);
+//		String.valueOf(1);
+//		Integer.toString(1);
 		
-		if(c==d){
-			System.out.println("같다");
-		}else{
-			System.out.println("다르다");
-		}
+		
 	}
 
 }
