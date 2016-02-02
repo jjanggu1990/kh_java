6f8514f1da5f604d42737d0e9eddc52d6cdc7f4b
kh <jjanggu1990@gmail.com>
2016-02-02 21:13:39 +0900

    0202 마지막 커밋

---
+++ workspace/ExJava/src/java_0202/InnerEx6.java
@@ -0,0 +1,19 @@
+package java_0202;
+
+class InnerEx6 {
+	Object iv = new Object() {
+		void method() {
+		}
+	}; // 익명클래스
+	static Object cv = new Object() {
+		void method() {
+		}
+	}; // 익명클래스
+
+	void myMethod() {
+		Object lv = new Object() {
+			void method() {
+			}
+		}; // 익명클래스
+	}
+}
