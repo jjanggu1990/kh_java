c2f21ccc2233978aaa41d607a7f9e93b3399d239
kh <jjanggu1990@gmail.com>
2016-02-05 17:05:39 +0900

    두번째 커밋

---
+++ workspace/ExJava/src/java_0205/JoinEx.java
@@ -0,0 +1,42 @@
+package java_0205;
+
+class MyRunnableTwo implements Runnable {
+	public void run() {
+		System.out.println("run");
+		first();
+	}
+
+	public void first() {
+		System.out.println("first");
+		second();
+	}
+
+	public void second() {
+		System.out.println("second");
+	}
+}
+class JoinEx {
+	public static void main(String[] args) {
+		System.out.println(
+				Thread.currentThread().getName() + " start");
+		Runnable r = new MyRunnableTwo();
+		Thread myThread = new Thread(r);
+		myThread.start();
+		try {
+			myThread.join();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+		System.out.println(
+				Thread.currentThread().getName() + " end");
+	}
+}
+
+
+
+
+
+
+
+
+
