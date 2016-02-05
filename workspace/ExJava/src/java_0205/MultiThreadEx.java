69fb3577aa67f3e9ad435bff4ba6a4db0b2406c9
kh <jjanggu1990@gmail.com>
2016-02-05 18:14:27 +0900

    저녁 전

--- workspace/ExJava/src/java_0205/MultiThreadEx.java
+++ workspace/ExJava/src/java_0205/MultiThreadEx.java
@@ -7,12 +7,12 @@ class MultiThreadEx implements Runnable {
 		for (int i = 0; i < 20; i++) {
 			first++;
 			second++;
-			System.out.printf("first : %d , " , first);
-			System.out.printf("second : %d , " , second);
-			System.out.printf("스레드 이름  : %s %n" , 
-					Thread.currentThread().getName());
+			System.out.printf("first : %d , ", first);
+			System.out.printf("second : %d , ", second);
+			System.out.printf("스레드 이름  : %s %n", Thread.currentThread().getName());
 		}
 	}
+
 	public static void main(String[] args) {
 		MultiThreadEx srt1 = new MultiThreadEx();
 		Thread firstThread = new Thread(srt1, "첫번째 스레드");
@@ -22,14 +22,3 @@ class MultiThreadEx implements Runnable {
 		secondThread.start();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
