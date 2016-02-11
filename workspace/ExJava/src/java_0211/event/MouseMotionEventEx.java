14e98d925a4d7d1a9af6167157a2ff317c5d2315
kh <jjanggu1990@gmail.com>
2016-02-11 21:09:49 +0900

    0211 마지막

--- workspace/ExJava/src/java_0211/event/MouseMotionEventEx.java
+++ workspace/ExJava/src/java_0211/event/MouseMotionEventEx.java
@@ -22,7 +22,52 @@ class MouseMotionEventEx extends Frame implements ActionListener, MouseMotionLis
 		
 		setBounds(300,100,500,600);
 		setVisible(true);
-		
+		addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowOpened");
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowIconified");
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowDeiconified");
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowDeactivated");
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowClosing");
+				System.exit(0);
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowClosed");
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("windowActivated");
+			}
+		});
 		exit.addActionListener(this);
 		addMouseMotionListener(this);
 	}
